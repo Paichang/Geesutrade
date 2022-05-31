2ae6869d48b705494d48653524785fa1e1b181f9
Paichang <a0983012133@gmail.com>
2022-06-01 00:31:09 +0800

    feat: add sales page

--- src/index.js
+++ src/index.js
@@ -15,6 +15,7 @@ import Landing from "views/Landing.js";
 import Profile from "views/Profile.js";
 import Artists from "views/Artists.js";
 import Artworks from "views/Artworks.js";
+import Sales from "views/Sales.js";
 import Index from "views/Index.js";
 
 ReactDOM.render(
@@ -28,6 +29,7 @@ ReactDOM.render(
       <Route path="/profile" exact component={Profile} />
       <Route path="/artists" exact component={Artists} />
       <Route path="/artworks" exact component={Artworks} />
+      <Route path="/sales" exact component={Sales} />
       <Route path="/" exact component={Index} />
       {/* add redirect for first page */}
       <Redirect from="*" to="/" />

c36ef32ed9b3ae09d396ab3fce4e727c6625b90f
Paichang <a0983012133@gmail.com>
2022-06-01 02:55:38 +0800

    feat: add report

--- src/components/Navbars/AuthNavbar.js
+++ src/components/Navbars/AuthNavbar.js
@@ -69,6 +69,14 @@ export default function Navbar(props) {
               </li>
               <li className="flex items-center">
                 <a
+                  className="lg:text-white lg:hover:text-blueGray-200 text-blueGray-700 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
+                  href="/report"
+                >
+                  Report
+                </a>
+              </li>
+              <li className="flex items-center">
+                <a
                   className="lg:text-white lg:hover:text-blueGray-200 text-xs font-bold uppercase px-4 py-2 rounded shadow hover:shadow-lg outline-none focus:outline-none lg:mr-1 lg:mb-0 ml-3 mb-3 ease-linear transition-all duration-150"
                   type="button"
                   href="/auth/register"
